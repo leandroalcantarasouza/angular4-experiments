5bb563e34ed26bf0364c69854b4523d5cfd1a0a8
Leandro Souza <leandro.alcantara@mprj.mp.br>
2017-08-07 19:10:11 -0300

    feat: Initial Commit based on joke list on book angular from theory to practice

---
+++ src/app/joke-form/joke-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Joke } from '../joke';
+
+@Component({
+  selector: 'joke-form',
+  templateUrl: './joke-form.component.html'
+})
+export class JokeFormComponent implements OnInit {
+  @Output() jokeCreated = new EventEmitter<Joke>();
+
+  createJoke(setup: string, punchline: string) {
+    this.jokeCreated.emit(new Joke(setup, punchline));
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/joke.ts
@@ -0,0 +1,13 @@
+export class Joke {
+  public hide: boolean;
+
+  constructor(private setup: string, private punchline: string) {
+    this.setup = setup;
+    this.punchline = punchline;
+    this.hide = true;
+  }
+
+  toggle() {
+    this.hide = !this.hide;
+  }
+}
